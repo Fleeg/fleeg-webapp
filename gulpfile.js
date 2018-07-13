eb5865b291ea1050b118d1da8aa2e28b6312d565
Lucas Boscaini <lucasboscaini@gmail.com>
2018-07-13 18:19:07 -0300

    Add bootstrap dependecies files

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
-const gulp = require('gulp');
-const pug = require('gulp-pug');
+const gulp = require('gulp')
+const pug = require('gulp-pug')
 
 // Copy css theme
-gulp.task('theme', () => 
+gulp.task('theme', () =>
   gulp.src('app/theme/*.css')
-      .pipe(gulp.dest('build/theme')));
+      .pipe(gulp.dest('build/theme')))
 
 // Generate html
-gulp.task('pug', ['theme'], () => 
+gulp.task('pug', () =>
   gulp.src('app/templates/*.pug')
       .pipe(pug())
-      .pipe(gulp.dest('build')));
+      .pipe(gulp.dest('build')))
+
+
+gulp.task('watch', () =>
+  gulp.watch('app/templates/*.pug', ['pug']))
+
+gulp.task('default', ['theme', 'pug', 'watch'])
